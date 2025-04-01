57921af4bcea857cad67eb1190414c65d96564bb
bit <secretbit007@gmail.com>
2025-04-01 22:49:17 +0900

    Update by date

--- naia.py
+++ naia.py
@@ -1,79 +1,61 @@
 from library import *
 
-def get_schedule_by_month(season_alias, year, month):
+def get_schedule_by_date(date: str):
     results = []
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
     }
-    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/scoreboard?y={year}&m={month:02d}', headers=headers)
+    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/scoreboard?d={date}', headers=headers)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        items = soup.find_all('div', class_='cal-show-more')
+        items = soup.find_all('div', class_='card')
         
         for item in items:
-            data_target = item.find('a').get('data-bs-target')
-            target_id = data_target.split('#')[1]
+            card_header = item.find('div', class_='card-header')
 
-            modal = soup.find('div', id=target_id)
+            game = {}
+            game['competition'] = card_header.find('div', class_='list-event-sport').text.strip()
+            game['round'] = card_header.find('div', class_='cal-status').text.strip()
+            game['state'] = 'Not Finished'
+            game['extid'] = ''
+            game['source'] = ''
+            game['type'] = 'Regular'
+            game['playDate'] = date
+            
+            card_body = item.find('div', class_='card-body')
+            links = card_body.find_all('a')
 
-            tbody = modal.find('tbody')
-            rows = tbody.find_all('tr')
+            for link in links:
+                if link.text.strip() == 'Box Score':
+                    game['state'] = 'Finished'
+                    game['source'] = 'https://naiastats.prestosports.com' + link['href']
 
-            for row in rows:
-                columns = row.find_all('td')
+                    game['extid'] = f"{link['href'].split('/')[-1].replace('.xml', '')}"
+                    break
+            
+            team_logos = card_body.find_all('img')
+            game['visitorTeam'] = {
+                'extid': team_logos[1].get('src').split('/')[-1].split('.')[0],
+                'name': team_logos[1].get('alt').strip()
+            }
 
-                season = columns[1].find('a').get('href').split('/')[-2]
+            game['homeTeam'] = {
+                'extid': team_logos[0].get('src').split('/')[-1].split('.')[0],
+                'name': team_logos[0].get('alt').strip()
+            }
 
-                if season != season_alias:
-                    continue
-
-                game = {}
-                game['competition'] = "Men's Basketball"
-                game['round'] = columns[0].text.strip()
-                game['state'] = 'Not Finished'
-                game['extid'] = ''
-                game['source'] = ''
-                game['type'] = 'Regular'
-                game['playDate'] = datetime.strptime(modal.find('span', class_='date').text.strip(), "%A, %B %d, %Y").strftime('%Y-%m-%d')
-                
-                links = columns[2].find_all('a')
-
-                for link in links:
-                    if link.text.strip() == 'Box Score':
-                        game['state'] = 'Finished'
-                        game['source'] = 'https://naiastats.prestosports.com' + link['href']
-
-                        if season_alias[-1] == 'p':
-                            game['extid'] = f"p-{link['href'].split('/')[-1].replace('.xml', '')}"
-                        elif season_alias[-1] == 'c':
-                            game['extid'] = f"c-{link['href'].split('/')[-1].replace('.xml', '')}"
-                        else:
-                            game['extid'] = f"o-{link['href'].split('/')[-1].replace('.xml', '')}"
-                        break
-                
-                team_logos = columns[1].find_all('img')
-                game['visitorTeam'] = {
-                    'extid': team_logos[1].get('src').split('/')[-1].split('.')[0],
-                    'name': team_logos[1].get('alt').strip()
+            if game['state'] == 'Finished':
+                scores = card_body.find_all('div', class_='event-result')
+                game['visitorScores'] = {
+                    'final': scores[0].text.strip()
                 }
-
-                game['homeTeam'] = {
-                    'extid': team_logos[0].get('src').split('/')[-1].split('.')[0],
-                    'name': team_logos[0].get('alt').strip()
+                game['homeScores'] = {
+                    'final': scores[1].text.strip()
                 }
+                game['extid'] = f'{game["extid"]}-{game["round"]}'
 
-                if game['state'] == 'Finished':
-                    score = columns[1].find('span', class_='cal-event-result').text.strip().split(',')[1].strip()
-                    game['visitorScores'] = {
-                        'final': score.split('-')[1].strip()
-                    }
-                    game['homeScores'] = {
-                        'final': score.split('-')[0].strip()
-                    }
-                    game['extid'] = f'{game["extid"]}-{game["round"]}'
-
-                results.append(game)
+            results.append(game)
 
     return results
 
@@ -87,24 +69,9 @@ def get_schedule(season_alias):
     for entry in entries:
         date_str = entry['updated'].split('T')[0]
         date_list.append(date_str)
-
-    startDate = date_list[0]
-    endDate = date_list[-1]
-
-    cur_date = datetime.strptime(startDate, '%Y-%m-%d').date()
-    end = datetime.strptime(endDate, '%Y-%m-%d').date()
-
-    month_list = []
-
-    while cur_date <= end:
-        month_list.append(cur_date)
-        cur_date += relativedelta(months=1)
-
-    if month_list[-1].month != end.month:
-        month_list.append(end)
-
-    for month in month_list:
-        results.extend(get_schedule_by_month(season_alias, month.year, month.month))
+    
+    for date in date_list:
+        results.extend(get_schedule_by_date(date))
 
     return results
 
@@ -114,10 +81,10 @@ def get_boxscore(extid, season_alias):
     }
     info = {}
     info['extid'] = extid
-    info['source'] = f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/boxscores/{extid.split("-")[1]}.xml?view=boxscore'
+    info['source'] = f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/boxscores/{extid.split("-")[0]}.xml?view=boxscore'
     info['type'] = extid.split('-')[-1]
 
-    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/boxscores/{extid.split("-")[1]}.xml?view=boxscore', headers=headers)
+    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/boxscores/{extid.split("-")[0]}.xml?view=boxscore', headers=headers)
 
     if response.status_code == 200:
         info['playDate'] = datetime.strftime(datetime.strptime(info['extid'].split('-')[1].split('_')[0], '%Y%m%d'), '%Y-%m-%d')
@@ -145,54 +112,30 @@ def get_boxscore(extid, season_alias):
         except:
             info['homeTeam']['extid'] = '-'
 
-        if extid[0] == 'o':
-            info['homeScores'] = {
-                'QT1': rows[2].find_all('td')[0].text,
-                'QT2': rows[2].find_all('td')[1].text,
-                'QT3': 0,
-                'QT4': 0,
-                'extra': 0,
-                'final': rows[2].find_all('td')[-1].text
-            }
+        info['homeScores'] = {
+            'QT1': rows[2].find_all('td')[0].text,
+            'QT2': rows[2].find_all('td')[1].text,
+            'QT3': 0,
+            'QT4': 0,
+            'extra': 0,
+            'final': rows[2].find_all('td')[-1].text
+        }
 
-            extra_scores = rows[2].find_all('td')[2:-1]
-        else:
-            info['homeScores'] = {
-                'QT1': rows[2].find_all('td')[0].text,
-                'QT2': rows[2].find_all('td')[1].text,
-                'QT3': rows[2].find_all('td')[2].text,
-                'QT4': rows[2].find_all('td')[3].text,
-                'extra': 0,
-                'final': rows[2].find_all('td')[-1].text
-            }
-
-            extra_scores = rows[2].find_all('td')[4:-1]
+        extra_scores = rows[2].find_all('td')[2:-1]
 
         for score in extra_scores:
             info['homeScores']['extra'] += int(score.text)
 
-        if extid[0] == 'o':
-            info['visitorScores'] = {
-                'QT1': rows[1].find_all('td')[0].text,
-                'QT2': rows[1].find_all('td')[1].text,
-                'QT3': 0,
-                'QT4': 0,
-                'extra': 0,
-                'final': rows[1].find_all('td')[-1].text
-            }
+        info['visitorScores'] = {
+            'QT1': rows[1].find_all('td')[0].text,
+            'QT2': rows[1].find_all('td')[1].text,
+            'QT3': 0,
+            'QT4': 0,
+            'extra': 0,
+            'final': rows[1].find_all('td')[-1].text
+        }
 
-            extra_scores = rows[1].find_all('td')[2:-1]
-        else:
-            info['visitorScores'] = {
-                'QT1': rows[1].find_all('td')[0].text,
-                'QT2': rows[1].find_all('td')[1].text,
-                'QT3': rows[1].find_all('td')[2].text,
-                'QT4': rows[1].find_all('td')[3].text,
-                'extra': 0,
-                'final': rows[1].find_all('td')[-1].text
-            }
-
-            extra_scores = rows[1].find_all('td')[4:-1]
+        extra_scores = rows[1].find_all('td')[2:-1]
 
         for score in extra_scores:
             info['visitorScores']['extra'] += int(score.text)
@@ -207,12 +150,7 @@ def get_boxscore(extid, season_alias):
             stat['team'] = info['homeTeam']['extid']
 
             try:
-                if extid[0] == 'p':
-                    stat['player_extid'] = f"p-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
-                elif extid[0] == 'c':
-                    stat['player_extid'] = f"c-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
-                else:
-                    stat['player_extid'] = f"o-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
+                stat['player_extid'] = f"{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
             except TypeError:
                 stat['player_extid'] = ''
             
@@ -275,12 +213,7 @@ def get_boxscore(extid, season_alias):
             stat['team'] = info['visitorTeam']['extid']
 
             try:
-                if extid[0] == 'p':
-                    stat['player_extid'] = f"p-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
-                elif extid[0] == 'c':
-                    stat['player_extid'] = f"c-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
-                else:
-                    stat['player_extid'] = f"o-{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
+                stat['player_extid'] = f"{parse_qs(urlparse(row.th.a['href']).query)['id'][0]}"
             except TypeError:
                 stat['player_extid'] = ''
 
@@ -342,7 +275,7 @@ def get_player(extid, season_alias):
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     }
-    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/players?id={extid.split("-")[1]}', headers=headers)
+    response = requests.get(f'https://naiastats.prestosports.com/sports/mbkb/{season_alias}/players?id={extid}', headers=headers)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -374,23 +307,13 @@ def func_naia(args):
         args['extid'] = request.args.get('extid')
         args['season'] = request.args.get('season')
 
-        if args['extid'][0] == 'p':
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}p'
-        elif args['extid'][0] == 'c':
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}c'
-        else:
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}'
+        season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}'
 
         return get_boxscore(args['extid'], season_alias)
     elif args['f'] == 'player':
         args['extid'] = request.args.get('extid')
         args['season'] = request.args.get('season')
 
-        if args['extid'][0] == 'p':
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}p'
-        elif args['extid'][0] == 'c':
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}c'
-        else:
-            season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}'
+        season_alias = f'{args["season"]}-{str(int(args["season"]) + 1)[-2:]}'
         
         return get_player(args['extid'], season_alias)

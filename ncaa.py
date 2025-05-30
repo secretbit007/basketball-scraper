957d1ce20f27ad8dc1931ce90c4885fce0190a15
bit <secretbit007@gmail.com>
2025-05-31 01:07:24 +0900

    Update headers

--- ncaa.py
+++ ncaa.py
@@ -432,6 +432,7 @@ def func_ncaa(args, seasonDivisionID):
         info['lastname'] = name.split(' ')[1]
         
         headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
         }
         response = requests.get(f'https://stats.ncaa.org/teams/{team}/roster', headers=headers)
